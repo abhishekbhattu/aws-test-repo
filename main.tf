4d57d725450396afb1ee5770bca531a6073d805a
Abhishek Bhattu <47747557+abhishekbhattu@users.noreply.github.com>
2025-11-28 23:22:00 +0530

    Update ami-id

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_security_group" "ec2_sg" {
 
 #  EC2 Instance
 resource "aws_instance" "my_ec2" {
-  ami                    = "ami-0c1a7f89451184c8b" # Amazon Linux 2 (ap-south-1)
+  ami                    = "ami-0a9098891d675c629" # Amazon Linux 2 (ap-south-1)
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
